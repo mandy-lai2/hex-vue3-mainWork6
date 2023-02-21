3c902ecdeb8f672dff557783e9ad90432bf57a6f
DESKTOP-V8E7JBJ\Mandy <chjo2980@gmail.com>
2023-02-22 00:31:10 +0800

    router修

--- src/router/index.js
+++ src/router/index.js
@@ -48,7 +48,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   linkActiveClass: 'active',
   routes
 })
