749c5d94a97e16cc00dbaaab718bd0768b5be974
DESKTOP-V8E7JBJ\Mandy <chjo2980@gmail.com>
2023-02-22 00:38:14 +0800

    viteConfig修改

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
     })
   ],
-  base: '/vue-6mainWork/',
+  base: '/hex-vue3-mainWork6/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
