69430554f3cf2cb24722d31b4f0e0ed68e910214
DESKTOP-V8E7JBJ\Mandy <chjo2980@gmail.com>
2023-02-22 00:16:57 +0800

    deploysh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/mandy-lai2/vite-deploy-practise2.git main:gh-pages
+git push -f https://github.com/mandy-lai2/hex-vue3-mainWork6.git main:gh-pages
 
 cd -
